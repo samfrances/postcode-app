94723ab22b105735ead2e193a60f8916aa656a14
Sam Frances <sam@samfrances.uk>
2021-09-21 17:59:26 +0100

    Improve CSS somewhat

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import {
   useParams
 } from "react-router-dom";
 import { PostCode } from "./types";
-import './App.css';
+import "./vendor/Skeleton-2.0.4/css/normalize.css";
+import "./vendor/Skeleton-2.0.4/css/skeleton.css";
 import NearestPostCodesContainer from './containers/NearestPostCodesContainer';
 import PostCodeInfoContainer from './containers/PostCodeInfoContainer';
 
@@ -26,14 +27,18 @@ function App() {
 }
 
 function Home() {
-  return <h2>Home</h2>;
+  return (
+    <div className="container">
+      <h2>Home</h2>
+    </div>
+  );
 }
 
 function PostCodePage() {
   const params = useParams<{ postcode: string }>();
   const postcode = PostCode.create(params.postcode)
   return (
-    <div>
+    <div className="container">
       <PostCodeInfoContainer postcode={postcode} />
       <NearestPostCodesContainer postcode={postcode} />
     </div>
