820d4e9165f9ed45e4fc77cbe1a84d036bbb227e
Sam Frances <sam@samfrances.uk>
2021-09-21 08:40:26 +0100

    Set up very basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/postcode/:postcode">
+        <PostCode />
+      </Route>
+    </Switch>
+  </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function PostCode() {
+  const { postcode } = useParams<{ postcode: string }>();
+  return (
+    <div>
+      <h2>Postcode {postcode}</h2>
     </div>
   );
 }
+
 
 export default App;
